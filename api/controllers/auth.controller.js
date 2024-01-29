3d795ea9a87f01b62047bd8aabac4c630dfd5f2a
vikalp <avikalp233@gmail.com>
2024-01-29 21:40:55 +0530

    Create signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../modals/user.modal.js";
 import bcryptjs from "bcryptjs"
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken"
 
 export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
@@ -21,6 +22,50 @@ export const signup = async (req, res, next) => {
     try {
         await newUser.save()
         res.json("Signup successful")
+    } catch (error) {
+        next(error)
+    }
+}
+
+
+export const signin = async (req, res, next) => {
+
+    const { email, password } = req.body;
+
+    if(!email || !password || email==="" || password==="")
+    {
+        next(errorHandler(400, "All fields are required"))
+    }
+
+    try {
+        const validUser = await User.findOne({ email })
+        if(!validUser)
+        {
+            return next(errorHandler(404, "Wrong Credentials"));
+        }
+
+        const validPassword = bcryptjs.compareSync(password, validUser.password)
+
+        if(!validPassword)
+        {
+            return next(errorHandler (400, "Wrong Credentials"))
+        }
+
+        //if everything went correct just create the cookie
+        const token = jwt.sign({
+            id: validUser._id
+        },process.env.JWT_SECRET)
+
+        const { password: pass, ...rest } = validUser._doc
+
+
+        res
+        .status(200)
+        .cookie("access_token", token, { 
+            httpOnly: true
+        })
+        .json(rest)
+
     } catch (error) {
         next(error)
     }
