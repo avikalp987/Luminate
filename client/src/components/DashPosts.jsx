8fa6502a60c5f056bcd7eb8c0dc1368fff329c5d
vikalp <avikalp233@gmail.com>
2024-02-02 18:33:46 +0530

    Add show more functionality

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -7,6 +7,7 @@ export default function DashPosts() {
   const { currentUser } = useSelector((state) => state.user)
 
   const [userPosts, setUserPosts] = useState([])
+  const [showMore, setshowMore] = useState(true);
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -16,6 +17,10 @@ export default function DashPosts() {
         if(res.ok)
         {
           setUserPosts(data.posts)
+          if(data.posts.length<9)
+          {
+            setshowMore(false)
+          }
         }
       } catch (error) {
         console.log(error.message)
@@ -27,6 +32,28 @@ export default function DashPosts() {
     }
 
   }, [currentUser._id])
+
+
+  const handleShowMore = async () => {
+    const startIndex = userPosts.length;
+
+    try {
+      const res = await fetch(`/api/post/getposts?userId=${currentUser._id}&startIndex=${startIndex}`)
+
+      const data = await res.json()
+
+      if(res.ok)
+      {
+        setUserPosts((prev) => [...prev, ...data.posts])
+        if(res.posts.length<9)
+        {
+          setshowMore(false)
+        }
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
   return (
     <div className='table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500'>
@@ -87,6 +114,14 @@ export default function DashPosts() {
 
             ))}
           </Table>
+
+          {
+            showMore && (
+              <button onClick={handleShowMore} className='w-full text-teal-500 self-center text-sm py-7'>
+                Show more
+              </button>
+            )
+          }
         </>
       ) : (
         <p>You have no posts yet!</p>
