c05197127c5b7c424895e50bb97559121dbd86e7
vikalp <avikalp233@gmail.com>
2024-02-03 15:41:23 +0530

    Add call to action to the post page

---
+++ client/src/components/CallToAction.jsx
@@ -0,0 +1,26 @@
+import { Button } from 'flowbite-react'
+import React from 'react'
+
+export default function CallToAction() {
+  return (
+    <div className="flex-col flex sm:flex-row p-3 border border-teal-500 justify-center items-center rounded-tl-3xl rounded-br-3xl text-center">
+        <div className='flex-1 justify-center flex flex-col'>
+            <h2 className='text-2xl'>
+                Check my Github!
+            </h2>
+            <p className='text-gray-500 my-2'>
+                Some of the amazing web development projects from youtube.
+            </p>
+
+            <Button gradientDuoTone={"purpleToPink"} className='rounded-tl-xl rounded-bl-none'>
+                <a href='https://github.com/avikalp987' target='_blank' rel='noopener noreferrer'>
+                    Let's Visit
+                </a>
+            </Button>
+        </div>
+        <div className="p-7 flex-1">
+            <img src={"https://i.pinimg.com/474x/e7/96/a1/e796a1c4f2777caf462636e4f7dce241.jpg"} alt="cover_image" />
+        </div>
+    </div>
+  )
+}
--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -1,6 +1,7 @@
 import { Button, Spinner } from 'flowbite-react';
 import React, { useEffect, useState } from 'react'
 import { Link, useParams } from 'react-router-dom'
+import CallToAction from '../components/CallToAction';
 
 export default function PostPage() {
 
@@ -80,7 +81,13 @@ export default function PostPage() {
         </div>
 
 
-        <div className="p-3 max-w-2xl mx-auto w-full post-content" dangerouslySetInnerHTML={{__html: post&&post.content}}></div>
+        <div className="p-3 max-w-2xl mx-auto w-full post-content" dangerouslySetInnerHTML={{__html: post&&post.content}}>
+        </div>
+
+
+        <div className="max-w-4xl mx-auto w-full">
+            <CallToAction />
+        </div>
     </main>
   )
 }
