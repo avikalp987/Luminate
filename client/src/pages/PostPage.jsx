6713890e64018ce95621c64aab852841f4053fc6
vikalp <avikalp233@gmail.com>
2024-02-04 11:52:17 +0530

    Add recent articles section to the post page

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react'
 import { Link, useParams } from 'react-router-dom'
 import CallToAction from '../components/CallToAction';
 import CommentSection from '../components/CommentSection';
+import PostCard from '../components/PostCard';
 
 export default function PostPage() {
 
@@ -12,6 +13,7 @@ export default function PostPage() {
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(false);
     const [post, setPost] = useState(null);
+    const [recentPosts, setRecentPosts] = useState(null);
 
 
     useEffect(() => {
@@ -46,6 +48,25 @@ export default function PostPage() {
         fetchPost();
 
     }, [postSlug])
+
+
+    useEffect(() => {
+        try {
+            const fetchRecentPosts = async () => {
+                const res = await fetch(`/api/post/getposts?limit=3`);
+                const data = await res.json()
+
+                if(res.ok)
+                {
+                    setRecentPosts(data.posts)
+                }
+            }
+
+            fetchRecentPosts()
+        } catch (error) {
+            console.log(error)
+        }
+    }, [])
 
 
   if(loading)
@@ -85,13 +106,26 @@ export default function PostPage() {
         <div className="p-3 max-w-2xl mx-auto w-full post-content" dangerouslySetInnerHTML={{__html: post&&post.content}}>
         </div>
 
-
         <div className="max-w-4xl mx-auto w-full">
             <CallToAction />
         </div>
 
-
         <CommentSection postId={post && post._id}/>
+
+        <div className='flex flex-col justify-center items-center mb-5'>
+            <h1 className='text-xl mt-5'>Recent Articles</h1>
+            <div className='flex flex-wrap gap-5 mt-5 justify-center'>
+                {
+                    recentPosts && recentPosts.map((post) => (
+                        <PostCard 
+                            key={post._id}
+                            post={post}
+                        />
+                    ))
+                }
+            </div>
+        </div>
+
     </main>
   )
 }
