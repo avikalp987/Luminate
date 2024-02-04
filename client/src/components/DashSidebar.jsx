5c473697f08199d238ce291a210865a1ffd09428
vikalp <avikalp233@gmail.com>
2024-02-04 13:58:28 +0530

    Add dashboard overview to the admin dashboard

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { Sidebar }  from "flowbite-react"
-import { HiAnnotation, HiArrowSmRight, HiDocumentText, HiOutlineUserGroup, HiUser } from "react-icons/hi"
+import { HiAnnotation, HiArrowSmRight, HiChartPie, HiDocumentText, HiOutlineUserGroup, HiUser } from "react-icons/hi"
 import { Link, useLocation } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { signoutSuccess } from '../redux/user/userSlice.js'
@@ -53,6 +53,18 @@ export default function DashSidebar() {
         <Sidebar.Items>
 
             <Sidebar.ItemGroup className='flex flex-col gap-1'>
+
+                {currentUser && currentUser.isAdmin && (
+                  <Link to={"/dashboard?tab=dash"}>
+                  <Sidebar.Item
+                    active={tab==="dash"}
+                    icon={HiChartPie}
+                    as="div"
+                  >
+                    Dashboard
+                  </Sidebar.Item>
+                  </Link>
+                )}
 
                 <Link to={"/dashboard?tab=profile"}>
                   <Sidebar.Item 
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -5,6 +5,7 @@ import DashProfile from '../components/DashProfile'
 import DashPosts from '../components/DashPosts'
 import DashUsers from '../components/DashUsers'
 import DashComments from '../components/DashComments'
+import DashboardComp from '../components/DashboardComp'
 
 export default function Dashboard() {
 
@@ -42,6 +43,9 @@ export default function Dashboard() {
 
       {/* comments */}
       {tab==="comments" && <DashComments />}
+
+      {/* Dashboard */}
+      {tab==="dash" && <DashboardComp/>}
     </div>
   )
 }
