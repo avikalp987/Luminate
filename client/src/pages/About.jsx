ec2914719425539c08c6220285b5dee2285f40e9
vikalp <avikalp233@gmail.com>
2024-02-05 11:31:49 +0530

    Complete about and project pages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,19 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='min-h-screen flex items-center justify-center'>
+      <div className="max-w-2xl mx-auto p-3 text-center">
+        <div className="">
+          <h1 className='text-3xl font-semibold text-center my-7'>About Luminate</h1>
+          <div className="text-md text-gray-500 flex flex-col gap-6">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint cupiditate minima facilis recusandae in nihil, ducimus blanditiis, suscipit, quisquam ullam eveniet ipsa est nisi amet quae consequatur exercitationem quo minus!</p>
+
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro impedit praesentium sed laborum sequi. Debitis fuga eos corrupti dicta nostrum.</p>
+
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio distinctio atque vel hic commodi repudiandae cumque eaque laudantium dicta neque ut voluptates esse fuga quo provident, vero ducimus placeat? Commodi.</p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
