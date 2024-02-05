0e1d9d2dae433511520b7113f0501a8c32ed52ee
vikalp <avikalp233@gmail.com>
2024-02-05 11:39:52 +0530

    Add pre installation

--- api/index.js
+++ api/index.js
@@ -5,8 +5,8 @@ import userRouter from "./routes/user.route.js"
 import authRouter from "./routes/auth.route.js"
 import postRouter from "./routes/post.route.js"
 import commentRouter from "./routes/comment.route.js"
-
 import cookieParser from "cookie-parser"
+import path from "path"
 
 //configuring the dotenv
 dotenv.config()
@@ -21,12 +21,14 @@ mongoose
     console.log(err)
 })
 
+
+const __dirname = path.resolve()
+
 //creating the application
 const app = express()
 
 //allowing the backend to use the json
 app.use(express.json())
-
 app.use(cookieParser())
 
 //listening to the port
@@ -39,6 +41,13 @@ app.use("/api/user", userRouter)
 app.use("/api/auth", authRouter)
 app.use("/api/post", postRouter)
 app.use("/api/comment", commentRouter)
+
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'))
+})
 
 
 app.use((err, req, res, next) => {
